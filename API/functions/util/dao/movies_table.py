5352678074469c9878070b113fe14e86efdebc51
maejimafumika <maejimafumika@maejimafumikanoMacBook-Pro.local>
2022-04-27 15:41:45 +0900

    add put functions to movies_table

--- API/functions/util/dao/movies_table.py
+++ API/functions/util/dao/movies_table.py
@@ -5,7 +5,7 @@ from util.model.errors import TableError
 
 class MoviesTable:
     __table:any
-    __primary_name = "movieId"
+    __primarykey_name = "movieId"
 
     def __init__(self) -> None:
         table_name:str = os.environ.get("MOVIES_TABLE")
@@ -16,3 +16,74 @@ class MoviesTable:
             dynamodb = boto3.resource('dynamodb')
         self.__table = dynamodb.Table(table_name)
 
+    def get_movie_by_movie_id(self, movie_id:str):
+        try:
+            response = self.__table.get_item(Key={
+                self.__primarykey_name:movie_id
+            })
+        except ClientError as e:
+            raise TableError(__name__, e.response['Error']['Code'], e.response["Error"]["Message"])
+        else:
+            return response.get('Item',{})
+
+    def add_image_url_to_movie(self, movie_id:str, image_url:str="Not exists"):
+        try:
+            response = self.__table.update_item(
+            Key={
+                self.__primarykey_name: movie_id,
+            },
+            UpdateExpression="set imageUrl=:image_url",
+            ExpressionAttributeValues={':image_url': image_url},
+            ReturnValues="UPDATED_NEW"
+        )
+        except ClientError as e:
+            raise TableError(__name__, e.response['Error']['Code'], e.response["Error"]["Message"])
+        else:
+            return response
+
+    def add_video_url_to_movie(self, movie_id:str, video_url:str="Not exists"):
+        try:
+            response = self.__table.update_item(
+            Key={
+                self.__primarykey_name: movie_id,
+            },
+            UpdateExpression="set videoUrl=:video_url",
+            ExpressionAttributeValues={':video_url': video_url},
+            ReturnValues="UPDATED_NEW"
+        )
+        except ClientError as e:
+            raise TableError(__name__, e.response['Error']['Code'], e.response["Error"]["Message"])
+        else:
+            return response
+    
+    def add_description_to_movie(self, movie_id:str, description:str="Not exists"):
+        try:
+            response = self.__table.update_item(
+            Key={
+                self.__primarykey_name: movie_id,
+            },
+            UpdateExpression="set description=:description",
+            ExpressionAttributeValues={':description': description},
+            ReturnValues="UPDATED_NEW"
+        )
+        except ClientError as e:
+            raise TableError(__name__, e.response['Error']['Code'], e.response["Error"]["Message"])
+        else:
+            return response
+    
+    def add_nearest_movie_ids_to_movie(self, movie_id:str, nearest_movie_ids:str):
+        try:
+            response = self.__table.update_item(
+            Key={
+                self.__primarykey_name: movie_id,
+            },
+            UpdateExpression="set nearestMovieIds=:nearest_movie_ids",
+            ExpressionAttributeValues={':nearest_movie_ids': nearest_movie_ids},
+            ReturnValues="UPDATED_NEW"
+        )
+        except ClientError as e:
+            raise TableError(__name__, e.response['Error']['Code'], e.response["Error"]["Message"])
+        else:
+            return response
+
+    
